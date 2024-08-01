c76c418a1a8ce816c903d2e0221398beb4d9aba2
RamzyAR7 <rameya683@gmail.com>
2024-08-01 23:25:06 +0300

    add v0

---
+++ 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0. Lockboxes mandatory file task 0
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    method that determines if all the boxes can be opened.
+    """
+    n = len(boxes)
+    unlocked = set()
+    unlocked.add(0)
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key not in unlocked and key < n:
+                unlocked.add(key)
+                stack.append(key)
+
+    return len(unlocked) == n
+    
